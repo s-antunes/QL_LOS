6282fb1de42ba25787c90124ed302755783449ca
Samuel <Samuel@DESKTOP-879L5QS>
2020-06-23 11:45:36 +0200

    Finalisation de la carte

--- Robiot/pch.h
+++ Robiot/pch.h
@@ -11,9 +11,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include "src/SCoordinates/SCoordinates.h"
-#include "src/SCost/SCost.h"
 #include "src/CArbre/CArbre.h"
-#include "src/CRobiot/CRobiot.h"
 #include "src/CForet/CForet.h"
 
 using std::cout;
